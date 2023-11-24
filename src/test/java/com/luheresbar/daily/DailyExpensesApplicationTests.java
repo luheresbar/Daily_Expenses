20ffc74aa64b12c200dadf43ddaa50df4b5a591c
Luis H. Espinosa <luheresbar@gmail.com>
2023-11-24 13:47:44 -0500

    Primer commit de la estructura del proyecto DAily-Expenses

---
+++ src/test/java/com/luheresbar/daily/DailyExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luheresbar.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
