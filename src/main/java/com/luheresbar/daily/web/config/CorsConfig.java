53ddd5f70216f608a0f11babfa08e6708825e23c
luheresbar <luheresbar@gmail.com>
2024-02-16 23:55:28 -0500

    ajuste de cors config all allowed

--- src/main/java/com/luheresbar/daily/web/config/CorsConfig.java
+++ src/main/java/com/luheresbar/daily/web/config/CorsConfig.java
@@ -15,8 +15,8 @@ public class CorsConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        // corsConfiguration.setAllowedOrigins(Arrays.asList("*")); // En este momento estamos permitiendo todas las peticiones que lleguen desde este origen.
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "URL: https://dailyexpenses-dev.web.app")); // En este momento estamos permitiendo todas las peticiones que lleguen desde este origen.
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*")); // En este momento estamos permitiendo todas las peticiones que lleguen desde todo los origenes.
+        // corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "URL: https://dailyexpenses-dev.web.app")); // En este momento estamos permitiendo todas las peticiones que lleguen desde este origen.
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // De esta forma se deja claro que metodos quiero que se consuman desde un origen cruzado.
         corsConfiguration.setAllowedHeaders(Arrays.asList("*")); // Esta es una configuracion mucho mas general, en donde se esta permitiendo todos lo encabezados que vengan a travez de cors.
 
